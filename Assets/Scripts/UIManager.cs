a853136fb09b8bc340a643031d5ad15e33da5da8
hyeyeoneeee <128965397+hyeyeoneeee@users.noreply.github.com>
2023-12-27 23:06:11 +0900

    버튼

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     public GameObject uiPanel; // 띄울 UI 패널
+    private int temCount; //온도숫자
+    private int HuCount; //습도숫자
+    public TextMeshProUGUI temText; //온도 텍스트
+    public TextMeshProUGUI HuText; //습도 텍스트
 
     void Start()
     {
         // 초기에는 UI를 비활성화
         uiPanel.SetActive(false);
+        temCount = 10;
+        HuCount = 60;
+        PlusTemCount();
+        PlusHuCount();
     }
 
     public void ShowUI()
@@ -19,10 +28,33 @@ public class UIManager : MonoBehaviour
         // 버튼을 누르면 호출되는 메서드
         uiPanel.SetActive(true);
     }
-
-    public void CloseUI()
+    public void CloseUI() 
     {
         // UI 닫기 버튼 등을 누를 때 호출되는 메서드
         uiPanel.SetActive(false);
     }
+    #region Count
+    public void PlusTemCount() //온도 증가 카운트 메서드
+    {
+        temCount += 5;
+        temText.text = temCount.ToString() + "°C";
+    }
+    public void PlusHuCount() //습도 증가 카운트 메서드
+    {
+        HuCount += 5;
+        HuText.text = HuCount.ToString() + "%";
+    }
+    public void MinusTemCount()
+    {
+        temCount -= 5;
+        temText.text = temCount.ToString() + "°C";
+    }
+    public void MinusHuCount()
+    {
+        HuCount -= 5;
+        HuText.text = HuCount.ToString() + "%";
+    }
+    #endregion
+
+
 }
